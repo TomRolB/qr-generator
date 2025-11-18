53b1f8a1cba9aa6ad97e0d4a0f1ead1c8497b7d7
tomas <tomasrolb@gmail.com>
2025-11-18 20:37:13 -0300

    feat(matrix-placement): Implement placement function with basic transitions

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,7 @@
 module Main where
 
 import Matrix.Matrix as Matrix
--- import Data.Set (Set, empty, insert, member)
-
--- mySet :: Set Int
--- mySet =
---     insert 1 $
---     insert 5 $
---     insert 3 empty
 
 main :: IO ()
 main = do
-    -- putStrLn $ "Is 3 a member? " ++ show (member 3 mySet)
-    -- putStrLn $ "Is 4 a member? " ++ show (member 4 mySet)
-    print (Matrix.placePixels [Black, White])
+    print (Matrix.placePixels [Black, White, White, Black, Black])
--- src/Matrix/Matrix.hs
+++ src/Matrix/Matrix.hs
@@ -7,6 +7,28 @@ data Pixel = Black | White deriving (Show)
 type Coords = (Int, Int)
 type Matrix = Map Coords Pixel
 
+initialState :: (Matrix, Coords, Transition)
+initialState = (Map.empty, (5, 5), placeNext)
+
 placePixels :: [Pixel] -> Matrix
-placePixels pixelStream = foldl placeModule Map.empty (zip [0..] pixelStream) where
-    placeModule map' (index, pixel) = Map.insert (index, index) pixel map' 
+placePixels pixelStream = first $ foldl placePixel initialState pixelStream
+
+placePixel :: (Matrix, Coords, Transition) -> Pixel -> (Matrix, Coords, Transition)
+placePixel (map', coords, transition) pixel = (updatedMap, nextCoords, nextTransition) where
+    updatedMap = Map.insert coords pixel map'
+    (nextCoords, nextTransition) = runTransition transition coords
+
+newtype Transition = Transition { runTransition :: Coords -> (Coords, Transition) }
+
+placeHere :: Transition
+placeHere = Transition (\(row, col) -> ((row, col), placeNext))
+
+placeNext :: Transition
+placeNext = Transition (\(row, col) -> ((row, col - 1), placeDiagonally))
+
+placeDiagonally :: Transition
+placeDiagonally = Transition (\(row, col) -> ((row - 1, col + 1), placeNext))
+
+
+first :: (a, b, c) -> a
+first (a, _, _) = a
