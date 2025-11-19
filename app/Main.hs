509d7d1cd3165f8b9da87d4b599ca5ebaa8362dd
tomas <tomasrolb@gmail.com>
2025-11-19 17:21:31 -0300

    feat(matrix-placement): Implement all transitions and boundary checking

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import Matrix.Matrix as Matrix
 
 main :: IO ()
 main = do
-    print (Matrix.placePixels [Black, White, White, Black, Black])
+    print (Matrix.placePixels [Black, White, White, Black, Black] 5)
--- src/Matrix/Matrix.hs
+++ src/Matrix/Matrix.hs
@@ -1,34 +1,80 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 module Matrix.Matrix (Matrix, Pixel(..), placePixels) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 data Pixel = Black | White deriving (Show)
+data Direction = Up | Down
 type Coords = (Int, Int)
-type Matrix = Map Coords Pixel
-
-initialState :: (Matrix, Coords, Transition)
-initialState = (Map.empty, (5, 5), placeNext)
-
-placePixels :: [Pixel] -> Matrix
-placePixels pixelStream = first $ foldl placePixel initialState pixelStream
-
-placePixel :: (Matrix, Coords, Transition) -> Pixel -> (Matrix, Coords, Transition)
-placePixel (map', coords, transition) pixel = (updatedMap, nextCoords, nextTransition) where
-    updatedMap = Map.insert coords pixel map'
-    (nextCoords, nextTransition) = runTransition transition coords
-
-newtype Transition = Transition { runTransition :: Coords -> (Coords, Transition) }
-
-placeHere :: Transition
-placeHere = Transition (\(row, col) -> ((row, col), placeNext))
-
-placeNext :: Transition
-placeNext = Transition (\(row, col) -> ((row, col - 1), placeDiagonally))
-
-placeDiagonally :: Transition
-placeDiagonally = Transition (\(row, col) -> ((row - 1, col + 1), placeNext))
+data Matrix = Matrix { pixels :: Map Coords Pixel, size :: Int } deriving (Show)
+data PlacementState = PlacementState {
+    matrix :: Matrix,
+    coords :: Coords,
+    transition :: Transition
+}
 
 
-first :: (a, b, c) -> a
-first (a, _, _) = a
+getInitialState :: Int -> PlacementState
+getInitialState size = PlacementState {
+    matrix = Matrix { pixels = Map.empty, size },
+    coords = (size, size),
+    transition = placeLeft placeZigZaggingUpwards
+}
+
+placePixels :: [Pixel] -> Int -> Matrix
+placePixels pixelStream size = matrix $ foldl placePixel initialState pixelStream where
+    initialState = getInitialState size
+
+placePixel :: PlacementState -> Pixel -> PlacementState
+placePixel(PlacementState { matrix = matrix, coords = coords, transition = transition }) pixel = PlacementState { matrix = updatedMatrix, coords = nextCoords, transition = nextTransition } where
+    updatedMatrix = matrix { pixels = Map.insert coords pixel matrix.pixels }
+    (nextCoords, nextTransition) = runTransition transition coords matrix.size
+
+newtype Transition = Transition { runTransition :: Coords -> Int -> (Coords, Transition) }
+
+--- Transition chains
+
+placeLeftTwice :: Transition -> Transition
+placeLeftTwice nextTransition = placeLeft $ placeLeft nextTransition
+
+placeZigZaggingUpwards :: Transition
+placeZigZaggingUpwards = placeUpAndRight $ placeLeft topBoundaryChecker
+
+placeZigZaggingDownwards :: Transition
+placeZigZaggingDownwards = placeDownAndRight $ placeLeft bottomBoundaryChecker
+
+--- Transition "proxies"
+
+topBoundaryChecker :: Transition
+topBoundaryChecker = Transition (\(row, col) limit -> 
+    if row <= 0
+    then runTransition (placeLeftTwice placeZigZaggingDownwards) (row, col) limit
+    else runTransition placeZigZaggingUpwards (row, col) limit
+  )
+
+bottomBoundaryChecker :: Transition
+bottomBoundaryChecker = Transition (\(row, col) limit -> 
+    if row >= limit
+    then runTransition (placeLeftTwice placeZigZaggingUpwards) (row, col) limit
+    else runTransition placeZigZaggingDownwards (row, col) limit
+  )
+
+--- Single transitions (curried)
+
+placeUpAndRight :: Transition -> Transition
+placeUpAndRight = placeWithDelta (-1) 1
+
+placeDownAndRight :: Transition  -> Transition
+placeDownAndRight = placeWithDelta 1 1
+
+
+placeLeft :: Transition -> Transition
+placeLeft = placeWithDelta 0 (-1)
+
+--- Base transition
+
+placeWithDelta :: Int -> Int -> Transition -> Transition
+placeWithDelta deltaX deltaY nextTransition =
+    Transition (\(row, col) _ -> ((row + deltaX, col + deltaY), nextTransition))
